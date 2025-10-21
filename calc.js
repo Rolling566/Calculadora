6de5e633060e3b2ea335923276b63667fd936c09
Rolling566 <159900273+Rolling566@users.noreply.github.com>
2025-10-21 20:20:33 +0100

    Enhance resolver function with input validation
    Added error handling and specific responses for certain inputs.

--- calc.js
+++ calc.js
@@ -1,15 +1,32 @@
 var tela = document.getElementById('tela')
 
 function mostrarnatela(input) {
-    tela.value += input;
+    if (tela.value === 'ERROR'){
+        tela.value = input
+    }   
+    else {
+        tela.value += input 
+    }
 }
 function limpartela() {
     tela.value = ''
 }
-function resolver() {
-    try {
-    tela.value = eval(tela.value)
-    } catch (error) {
-    tela.value = 'ERROR'
+function resolver() {    
+    if (tela.value == 0) {
+    tela.value = 'Escreva um operação'
+    }
+    else if (tela.value == '6+7') {
+        tela.value = 'SIXX SEVENNN'
+    }
+    else if (tela.value == '6+9') {
+        tela.value = 'SAFADO'
+    }
+    else {
+        try {
+        tela.value = eval(tela.value)
+        } 
+        catch (error) {
+        tela.value = 'ERROR'
+        }
     }
 }
